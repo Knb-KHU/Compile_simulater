970b72416e467d99b54809ba40ecdf09d18fdd58
Ko Eunbi <srain0723@khu.ac.kr>
2025-11-16 21:31:23 +0900

    Update comsimultest.py

--- comsimultest.py
+++ comsimultest.py
@@ -8,13 +8,13 @@ from compile_simulater_v2.so import Assemblylan
 class comsimultest(unittest.TestCase):
   def test_comp(self):
     comp=compilerhlan("1234", 0)
-    self.assertEqual(comp.get(), 1234)
+    self.assertEqual(comp.Get(), 1234)
   def test_interp(self):
     interp=interpreterhl("2345", 0)
-    self.assertEqual(interp.get(), 2345)
+    self.assertEqual(interp.Get(), 2345)
   def test_assembly(self):
       a = Assemblylan("456", 0)
-      self.assertEqual(a.get(), "456")
-      self.assertEqual(a.pre_get(), 0)
+      self.assertEqual(a.Get(), "456")
+      self.assertEqual(a.pre_Get(), 0)
 if __name__ == '__main__':
     unittest.main()
