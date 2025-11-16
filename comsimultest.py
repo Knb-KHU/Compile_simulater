2c25bbe185e17520e464c3b5fcdf1378ce5b28e7
Ko Eunbi <srain0723@khu.ac.kr>
2025-11-16 20:48:00 +0900

    Create comsimultest.py

---
+++ comsimultest.py
@@ -0,0 +1,8 @@
+#import unittest
+from compile_simulater_v2.cpp import interpreterhl
+
+class comsimultest(unittest.TestCase):
+def test_comp(self):
+i=interpreterhl()
+i interp("2345", 0);
+std::cout<<self.Get()<<std::endl
