3d66bf28b5e8fd0f652c92df0e86af6bb5246b38
Ko Eunbi <srain0723@khu.ac.kr>
2025-11-16 21:02:59 +0900

    Update comsimultest.py

--- comsimultest.py
+++ comsimultest.py
@@ -2,7 +2,15 @@
 from compile_simulater_v2.cpp import interpreterhl
 
 class comsimultest(unittest.TestCase):
-def test_comp(self):
-i=interpreterhl()
-i interp("2345", 0);
-std::cout<<self.Get()<<std::endl
+  def test_comp(self):
+    c= compilerhlan()
+    comp=c("1234", 0)
+    self.assertEqual(comp.get(), 1234)
+  def test_interp(self):
+    i=interpreterhl()
+    interp=i("2345", 0)
+    self.assertEqual(interp.get(), 2345)
+  def test_assembly(self):
+      a = Assemblylan("456", 0)
+      self.assertEqual(a.get(), "456")
+      self.assertEqual(a.pre_get(), 0)
